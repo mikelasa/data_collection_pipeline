6ea150491f768284fdf7c870fc7a04b1e7f5eac5
mikelasa <mlasa@mondragon.edu>
2025-09-05 12:48:15 +0200

    prueba commit

--- data_collection/src/impedance_control.cpp
+++ data_collection/src/impedance_control.cpp
@@ -17,32 +17,32 @@ int main ()
 {
     // launch the robot with the ip address of the robot
     franka::Robot robot("172.17.6.164");
-
-    // Compliance parameters
-    const double K_trans{0};
-    const double K_rot{0};
-    const double k_nullspace_trans{0};
-    const double k_nullspace_rot{0};
-
-    //create the impedance matrixes
-    Eigen::MatrixXd stiffness(6, 6), damping(6, 6);
-    stiffness.setZero();
-    stiffness.topLeftCorner(3, 3) << K_trans * Eigen::MatrixXd::Identity(3, 3);
-    stiffness.bottomRightCorner(3, 3) << K_rot * Eigen::MatrixXd::Identity(3, 3);
-    damping.setZero();
-    damping.topLeftCorner(3, 3) << 2.0 * sqrt(K_trans) * Eigen::MatrixXd::Identity(3, 3);
-    damping.bottomRightCorner(3, 3) << 2.0 * sqrt(K_rot) *Eigen::MatrixXd::Identity(3, 3);
-
-    // Nullspace parameters same as the compliance parameters
-    Eigen::MatrixXd null_stiffness(6, 6), null_damping(6, 6);
-    null_stiffness.setZero();
-    null_stiffness.topLeftCorner(3, 3) << k_nullspace_trans * Eigen::MatrixXd::Identity(3, 3);
-    null_stiffness.bottomRightCorner(3, 3) << k_nullspace_rot * Eigen::MatrixXd::Identity(3, 3);
-    null_damping.setZero();
-    null_damping.topLeftCorner(3, 3) << 2.0 * sqrt(k_nullspace_trans) * Eigen::MatrixXd::Identity(3, 3);
-    null_damping.bottomRightCorner(3, 3) << 2.0 * sqrt(k_nullspace_rot) * Eigen::MatrixXd::Identity(3, 3);
     
     try {
+
+        // Compliance parameters
+        const double K_trans{0};
+        const double K_rot{0};
+        const double k_nullspace_trans{0};
+        const double k_nullspace_rot{0};
+
+        //create the impedance matrixes
+        Eigen::MatrixXd stiffness(6, 6), damping(6, 6);
+        stiffness.setZero();
+        stiffness.topLeftCorner(3, 3) << K_trans * Eigen::MatrixXd::Identity(3, 3);
+        stiffness.bottomRightCorner(3, 3) << K_rot * Eigen::MatrixXd::Identity(3, 3);
+        damping.setZero();
+        damping.topLeftCorner(3, 3) << 2.0 * sqrt(K_trans) * Eigen::MatrixXd::Identity(3, 3);
+        damping.bottomRightCorner(3, 3) << 2.0 * sqrt(K_rot) *Eigen::MatrixXd::Identity(3, 3);
+
+        // Nullspace parameters same as the compliance parameters
+        Eigen::MatrixXd null_stiffness(6, 6), null_damping(6, 6);
+        null_stiffness.setZero();
+        null_stiffness.topLeftCorner(3, 3) << k_nullspace_trans * Eigen::MatrixXd::Identity(3, 3);
+        null_stiffness.bottomRightCorner(3, 3) << k_nullspace_rot * Eigen::MatrixXd::Identity(3, 3);
+        null_damping.setZero();
+        null_damping.topLeftCorner(3, 3) << 2.0 * sqrt(k_nullspace_trans) * Eigen::MatrixXd::Identity(3, 3);
+        null_damping.bottomRightCorner(3, 3) << 2.0 * sqrt(k_nullspace_rot) * Eigen::MatrixXd::Identity(3, 3);
         
         // Set additional parameters always before the control loop, NEVER in the control loop!
         // Set collision behavior.
@@ -55,21 +55,16 @@ int main ()
         robot.setCartesianImpedance({{3000, 3000, 3000, 300, 300, 300}});
 
         //set robot payload
-        robot.setLoad(
+        /*robot.setLoad(
             0.2,                                       // mass
             {0.0, 0.0, 0.1},                          // CoM in tool frame (example)
             {0.0, 0.0, 0.0,                            // 3x3 inertia matrix (flat array)
             0.0, 0.0, 0.0,
             0.0, 0.0, 0.0}
-        );
+        );*/
 
         // Load the robot model
         franka::Model model(robot.loadModel());
-        // Define the initial joint configuration
-        std::array<double, 7> q_goal = {{0, -M_PI_4, 0, -3 * M_PI_4, 0, M_PI_2, M_PI_4}};
-        // Set the motion generator with a speed of 0.5 and the goal joint configuration
-        // send robot to initial position through the motion generator
-        std::cout << "Finished moving to initial joint configuration." << std::endl;
         
         // Read the robot state once
         franka::RobotState initial_state = robot.readOnce();
@@ -92,11 +87,9 @@ int main ()
 
         // Define the impedance control callback
         std::function<franka::Torques(const franka::RobotState&, franka::Duration)>
-        impedance_control_callback = [&save_data, &model, &position_d, &orientation_d, &stiffness, 
-                                        &damping, &null_stiffness, &null_damping, &q_d, &robot](const franka::RobotState& robot_state,
+        impedance_control_callback = [&model, &stiffness, &damping, &null_stiffness, &null_damping, &q_d, &robot,
+                                      &position_d, &orientation_d](const franka::RobotState& robot_state,
                                         franka::Duration /*duration*/) -> franka::Torques {
-
-            franka::RobotState initial_state = robot.readOnce();
 
             // get state variables
             std::array<double, 7> coriolis_array = model.coriolis(robot_state);
@@ -121,7 +114,6 @@ int main ()
             Eigen::Matrix<double, 6, 1> error;
             error.head(3) << position - position_d;
 
-            
             // orientation error
             // "difference" quaternion
             if (orientation_d.coeffs().dot(orientation.coeffs()) < 0.0) {
@@ -141,21 +133,6 @@ int main ()
             tau_nullspace = nullspace_projector * (null_stiffness * (q_d - q) - null_damping * dq);
             tau_d << tau_task + tau_nullspace + coriolis;
 
-            //0 command for debugging
-            //tau_d = Eigen::VectorXd::Zero(7);
-
-            // Update data to print.
-            if (save_data.mutex.try_lock()) {
-                save_data.has_data = true;
-                save_data.O_T_EE = robot_state.O_T_EE;
-                save_data.q = robot_state.q;
-                save_data.dq = robot_state.dq;
-                save_data.tau_J = robot_state.tau_J;
-                save_data.O_F_ext_hat_K= robot_state.O_F_ext_hat_K; 
-                save_data.cartesian_contact = robot_state.cartesian_contact; // Assuming this is the contact force
-                save_data.mutex.unlock();
-            }
-
             std::array<double, 7> tau_d_array{};
             Eigen::VectorXd::Map(&tau_d_array[0], 7) = tau_d;
             //print the control command
@@ -166,17 +143,9 @@ int main ()
 
     robot.control(impedance_control_callback, false, 1000.0);
 
-    //end thread
-    running = false;
-
     } catch (const franka::Exception& e) {
-        running = false;
         std::cout << e.what() << std::endl;
         return -1;
-    }
-
-    if (print_thread.joinable()) {
-    print_thread.join();
     }
 
   return 0;
